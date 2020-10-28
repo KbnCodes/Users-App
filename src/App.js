bcb2a563d8a69797082e79086f204613842cddbe
kbncodes <muhdkbn@gmail.com>
2020-10-28 14:00:50 +0530

    User app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/pages/Home";
+import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import NotFound from "./components/pages/NotFound";
+import AddUser from "./components/pages/users/AddUser";
+import EditUser from "./components/pages/users/EditUser";
+import UserView from "./components/pages/users/UserView";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path={"/"} exact component={Home} />
+          <Route path={"/about"} exact component={About} />
+          <Route path={"/contact"} exact component={Contact}/>
+          <Route path={"/users/add"} component={AddUser}/>
+          <Route path={"/users/edit/:id"} component={EditUser}/>
+          <Route path={"/users/:id"} exact component={UserView} />
+
+          <Route component={NotFound}/>
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About () {
+    return (
+        <div className="container">
+            <div className="py-4">
+               <h1>about page</h1>
+               <p className="lead"> Dummy content</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default About
